cabd30d4360ffba20035fe1e753551c3054f65cf
davinoroes <davinoroes1918@gmail.com>
2026-05-05 13:02:57 -0300

    Paths relacionados a gênero

--- app/urls.py
+++ app/urls.py
@@ -20,6 +20,6 @@ from genre.views import GenreCreateListView,GenreRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genre/', GenreCreateListView.as_view(),name='create-list-genre')
+    path('genre/', GenreCreateListView.as_view(),name='create-list-genre'),
     path('genre/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='update-delete-genre'),
 ]
