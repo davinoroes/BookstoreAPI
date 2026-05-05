b8e3d4b600799093499af222ee0975d9352da006
davinoroes <davinoroes1918@gmail.com>
2026-05-05 11:37:46 -0300

    Criação serializer, e adição ao serializer class das views

--- genre/views.py
+++ genre/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework import generics
 from genre.models import Genre
+from genre.serializers import GenreModelSerializer
 
 class GenreCreateListView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
-    serializer_class = None
+    serializer_class = GenreModelSerializer
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Genre.objects.all()
-    serializer_class = None
+    serializer_class = GenreModelSerializer
