7d027a9cb6174bca996482d5e115cc687a55b1e1
davinoroes <davinoroes1918@gmail.com>
2026-05-07 13:31:14 -0300

    Att book serializers

--- books/serializers.py
+++ books/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from books.models import Book
 
 
-class BookSerializer(serializers.ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):  
     class Meta:
         model = Book
         fields = ('__all__')
