ccbaeef2519ad6a93c51cc85bd4e1cd6a82e9b20
davinoroes <davinoroes1918@gmail.com>
2026-05-09 19:00:25 -0300

    sobrescrita dos metodos get e post para impressao no terminal

--- genre/views.py
+++ genre/views.py
@@ -7,6 +7,15 @@ class GenreCreateListView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreModelSerializer
 
+    def get(self, request, *args, **kwargs):
+        print(f'Listando generos - Metodo: {request.method} - Path: {request.path}')
+        return super().get(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        print(f'Criando genero - Metodo: {request.method} - Path: {request.path} - Genero: {request.data}')
+        return super().post(request, *args, **kwargs)
+    
+
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Genre.objects.all()
