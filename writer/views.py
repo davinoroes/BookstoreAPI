2aa0ae44cd33598228e5963282ba7f34b36f0f13
davinoroes <davinoroes1918@gmail.com>
2026-05-09 19:25:03 -0300

    sobrescrita metodo get e post para impressao

--- writer/views.py
+++ writer/views.py
@@ -7,6 +7,13 @@ class WriterListCreateView(generics.ListCreateAPIView):
     queryset = Writer.objects.all()
     serializer_class = WriterSerializer
 
+    def get(self, request, *args, **kwargs):
+        print(f'listando escritores - Metodo: {request.method} - Path: {request.path}')
+        return super().get(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        print(f'criando escritor - Metodo: {request.method} - Path: {request.path} - Genero: {request.data}')
+        return super().post(request, *args, **kwargs)
 class WriterRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Writer.objects.all()
     serializer_class = WriterSerializer
